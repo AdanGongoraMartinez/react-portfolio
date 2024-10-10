be8b052a18e1fe88189fd06140be117bc90396db
Adan <adangonmar@gmail.com>
2024-10-09 20:29:23 -0600

    Updating Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import { getImageUrl } from "../../utils";
 export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>¡Hola! Soy Adán</h1>
-            <p className={styles.description}>Ingeniero Informático</p>
+            <h1 className={styles.title}>Adán Antonio Góngora Martínez</h1>
+            <p className={styles.description}>¡Hola! Soy ingeniero informático, tengo 22 años y me apasiona la programación.</p>
             <a href="mailto:21890241@ittizimin.edu.mx" className={styles.contactBtn}>
                 Contáctame
             </a>

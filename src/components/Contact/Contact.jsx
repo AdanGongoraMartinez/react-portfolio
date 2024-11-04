c553940efaac899941a72ceaf3f15919f49e46ac
adan-local <adangonmar@gmail.com>
2024-11-03 20:46:34 -0600

    hidding linkeding link and changing hover on hero contact btn

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,41 +4,32 @@ import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
-    return (
-        <footer id="contact" className={styles.container}>
-            <div className={styles.text}>
-                <h2>Contacto</h2>
-                <p>¡No dudes en enviarme un mensaje!</p>
-            </div>
-            
-            <ul className={styles.links}>
-                <li className={styles.link}>
-                    <img 
-                        src={getImageUrl("contact/emailIcon.png")} 
-                        alt="email icon" 
-                    />
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contacto</h2>
+        <p>¡No dudes en enviarme un mensaje!</p>
+      </div>
 
-                    <a href="21890241@ittizimin.edu.mx">21890241@ittizimin.edu.mx</a>
-                </li>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/emailIcon.png")}
+            alt="email icon"
+          />
 
-                <li className={styles.link}>
-                    <img 
-                        src={getImageUrl("contact/linkedinIcon.png")} 
-                        alt="Linkedin Icon" 
-                    />
+          <a href="21890241@ittizimin.edu.mx">21890241@ittizimin.edu.mx</a>
+        </li>
 
-                    <a href="linkedin">Linkedin</a>
-                </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/githubIcon.png")}
+            alt="Github icon"
+          />
 
-                <li className={styles.link}>
-                    <img 
-                        src={getImageUrl("contact/githubIcon.png")} 
-                        alt="Github icon" 
-                    />
-
-                    <a href="https://github.com/AdanGongoraMartinez">Github</a>
-                </li>
-            </ul>
-        </footer>
-    )
+          <a href="https://github.com/AdanGongoraMartinez">Github</a>
+        </li>
+      </ul>
+    </footer>
+  )
 };

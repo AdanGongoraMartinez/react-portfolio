2b174013c9636e7d12ebbf2be5dfad27f6d40df3
Adan Gongora <adangonmar@gmail.com>
2024-12-11 01:27:39 -0600

    Adding To-Do-List proyect

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,12 +10,15 @@ export const Projects = () => {
         <section className={styles.container} id="projects">
             <h2 className={styles.title}>Proyectos</h2>
             <p className={styles.nota}>
+                NOTA: Las aplicaciones pueden tardar en encender.
+            </p>
+            <p className={styles.nota}>
                 NOTA: Debido a un error en el SGDB implementado en la nube, los proyectos están temporalmente inoperativos.
             </p>
 
             <div className={styles.projects}>
-                {projects.map((project,id) => {
-                    return (<ProjectCard key={id} project={project}/>)
+                {projects.map((project, id) => {
+                    return (<ProjectCard key={id} project={project} />)
                 })}
             </div>
         </section>
